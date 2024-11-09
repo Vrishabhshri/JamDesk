7716f4a3d6f0b80b1be450fe54ee76f1c1105cdf
Vrishabhshri <vrishabhshri@gmail.com>
2024-11-09 15:42:11 -0500

    hiding scroll bar in list

---
+++ jam-desk/src/components/ListItemOptions/ListItemOptions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ListItemOptions = () => {
+
+    return (
+
+        <div className='list-item-options'>
+
+            
+
+        </div>
+
+    )
+
+}
+
+export default ListItemOptions;
